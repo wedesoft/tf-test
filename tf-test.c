b0eb1c837cd9a774acee3c55c765b2d1e1156399
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-11-08 21:37:50 +0000

    Instantiate placeholder

--- tf-test.c
+++ tf-test.c
@@ -13,8 +13,9 @@ int main() {
   printf("Hello from TensorFlow C library version %s\n", TF_Version());
   TF_Status *status = TF_NewStatus();
   TF_Graph *graph = TF_NewGraph();
-  TF_OperationDescription *descr = TF_NewOperation(graph, "Placeholder", "x");
-  TF_FinishOperation(descr, status);
+  TF_OperationDescription *desc = TF_NewOperation(graph, "Placeholder", "x");
+  TF_SetAttrType(desc, "dtype", TF_FLOAT);
+  TF_Operation *op = TF_FinishOperation(desc, status);
   CHECK(status);
   TF_SessionOptions *opts = TF_NewSessionOptions();
   TF_Session *session = TF_NewSession(graph, opts, status);
