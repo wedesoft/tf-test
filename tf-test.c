adc4e87a06db331680156040bb5b6bc48e7b50e5
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-11-09 12:55:50 +0000

    Trying to compute identity function

--- tf-test.c
+++ tf-test.c
@@ -19,10 +19,25 @@ int main() {
   CHECK(status);
   printf("operation name = %s\n", TF_OperationName(op));
   printf("operation type = %s\n", TF_OperationOpType(op));
+  printf("operation outpus = %d\n", TF_OperationNumOutputs(op));
   TF_SessionOptions *opts = TF_NewSessionOptions();
   TF_Session *session = TF_NewSession(graph, opts, status);
   CHECK(status);
 
+  TF_Output input;
+  input.oper = op;
+  input.index = 0;
+
+  TF_Tensor *output;
+
+  TF_Tensor *input_value = TF_AllocateTensor(TF_FLOAT, NULL, 0, 4);
+  ((float *)TF_TensorData(input_value))[0] = 3.0;
+
+  TF_SessionRun(session, NULL, &input, &input_value, 1, &input, &output, 1, NULL, 0, NULL, status);
+  CHECK(status);
+
+  TF_DeleteTensor(input_value);
+  TF_DeleteTensor(output);
   TF_DeleteSessionOptions(opts);
   TF_DeleteSession(session, status);
   CHECK(status);
