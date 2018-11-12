97670508026ba61514a6920dd79e23ab38ac3024
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-11-12 21:43:45 +0000

    List operations

--- parse.c
+++ parse.c
@@ -7,6 +7,8 @@ int main(void)
   TF_Buffer *buffer = TF_GetAllOpList();
   Tensorflow__OpList *op_list = tensorflow__op_list__unpack(NULL, buffer->length, buffer->data);
   printf("unpacked %d operations\n", op_list->n_op);
+  for (int i=0; i<op_list->n_op; i++)
+    printf("* %s\n", op_list->op[i]->name);
   tensorflow__op_list__free_unpacked(op_list, NULL);
   TF_DeleteBuffer(buffer);
   return 0;
